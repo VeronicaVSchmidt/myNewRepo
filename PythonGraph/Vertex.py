768c453b00c2cb088602609d5631c7cbb8bf189c
Ekinneberg <eric@surveyvitals.com>
2018-01-29 15:47:52 -0700

    This is my first commit for Graphs

---
+++ PythonGraph/Vertex.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 10:59:41 2017
+
+@author: Veronica 
+"""
+
+class Vertex:
+    def __init__(self,key):
+        self.id = key
+        self.connectedTo = {}
+
+    def addNeighbor(self,nbr,weight=0):
+        self.connectedTo[nbr] = weight
+
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+
+    def getConnections(self):
+        return self.connectedTo.keys()
+
+    def getId(self):
+        return self.id
+
+    def getWeight(self,nbr):
+        return self.connectedTo[nbr]
